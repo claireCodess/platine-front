f1e747249a4521b5e078bc9f09a797929191c4b4
Hunter Claire <claire.hunter@etudiant.univ-lille1.fr>
2018-03-03 01:31:58 +0100

    Détails d'un événement : transformation des données en données affichables dans la vue

--- app/src/main/java/huntermahroug/com/lille1campus/viewmodel/EventViewModel.java
+++ app/src/main/java/huntermahroug/com/lille1campus/viewmodel/EventViewModel.java
@@ -4,7 +4,13 @@ import android.app.Fragment;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 import huntermahroug.com.lille1campus.model.Event;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 
 /**
  * Created by Claire on 02/03/2018.
@@ -57,5 +63,72 @@ public class EventViewModel extends BaseObservable {
     public String getCategory() {
         return event.getCategory();
     } */
+
+    @Bindable
+    public String getNeedsResrvation() {
+        // Si le nombre de places est non nul, alors cet événement
+        // a besoin d'une réservation. On affiche alors "Sur réservation"
+        if(event.getTotalPlaces() != 0) {
+            return "Sur réservation";
+        } else {
+            // Sinon, on affiche "Sans réservation"
+            return "Sans réservation";
+        }
+    }
+
+    @Bindable
+    public int getShowReservation() {
+        // Si le nombre de places est non nul, alors cet événement
+        // a besoin d'une réservation et on affiche alors
+        // le texte pour indiquer le nombre de places restantes.
+        if(event.getTotalPlaces() != 0) {
+            return VISIBLE;
+        }
+        // Sinon, on n'affiche pas le texte
+        else {
+            return GONE;
+        }
+    }
+
+    @Bindable
+    public String getPlaces() {
+        // Si le nombre de places est non nul, alors cet événement
+        // a besoin d'une réservation et le texte contiendra le nombre
+        // de places restantes sur le nombre de places au total.
+        int totalPlaces = event.getTotalPlaces();
+        if(totalPlaces != 0) {
+            return "(Places disponibles : " + event.getAvailablePlaces()
+                    + " / " + totalPlaces + ")";
+        }
+        // Sinon, le texte n'est pas affiché
+        else {
+            return "N/A";
+        }
+    }
+
+    @Bindable
+    public String getPrice() {
+        int priceInCents = event.getPrice();
+        if(priceInCents == 0) {
+            return "Gratuit";
+        } else {
+            // Le prix est stocké dans la BDD en centimes,
+            // il faut le convertir en euros (avec virgule)
+            // avant de l'afficher.
+            double priceInEuros = priceInCents / 100.00;
+            NumberFormat form = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+            return form.format(priceInEuros);
+        }
+    }
+
+    @Bindable
+    public String getEmail() {
+        return event.getEmail();
+    }
+
+    @Bindable
+    public String getDescription() {
+        return event.getDescription();
+    }
 
 }
